3bd074ac90640ec80183efcbfaa60c5da0397ba7
Rajesh Ashok Jaiswal <rajeshjaiswal878@gmail.com>
2021-01-23 11:19:42 -0800

    Problem 1113 Reported Posts Solution

---
+++ Easy Problem/pb_1113_reported_posts.sql
@@ -0,0 +1,70 @@
+/*
+Table: Actions
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| user_id       | int     |
+| post_id       | int     |
+| action_date   | date    |
+| action        | enum    |
+| extra         | varchar |
++---------------+---------+
+There is no primary key for this table, it may have duplicate rows.
+The action column is an ENUM type of ('view', 'like', 'reaction', 'comment', 'report', 'share').
+The extra column has optional information about the action such as a reason for report
+or a type of reaction.
+
+
+Write an SQL query that reports the number of posts reported yesterday for each report reason.
+Assume today is 2019-07-05.
+
+The query result format is in the following example:
+
+Actions table:
++---------+---------+-------------+--------+--------+
+| user_id | post_id | action_date | action | extra  |
++---------+---------+-------------+--------+--------+
+| 1       | 1       | 2019-07-01  | view   | null   |
+| 1       | 1       | 2019-07-01  | like   | null   |
+| 1       | 1       | 2019-07-01  | share  | null   |
+| 2       | 4       | 2019-07-04  | view   | null   |
+| 2       | 4       | 2019-07-04  | report | spam   |
+| 3       | 4       | 2019-07-04  | view   | null   |
+| 3       | 4       | 2019-07-04  | report | spam   |
+| 4       | 3       | 2019-07-02  | view   | null   |
+| 4       | 3       | 2019-07-02  | report | spam   |
+| 5       | 2       | 2019-07-04  | view   | null   |
+| 5       | 2       | 2019-07-04  | report | racism |
+| 5       | 5       | 2019-07-04  | view   | null   |
+| 5       | 5       | 2019-07-04  | report | racism |
++---------+---------+-------------+--------+--------+
+
+Result table:
++---------------+--------------+
+| report_reason | report_count |
++---------------+--------------+
+| spam          | 1            |
+| racism        | 2            |
++---------------+--------------+
+Note that we only care about report reasons with non zero number of reports.
+
+Problem Link:https://leetcode.com/problems/reported-posts/
+*/
+
+# My Solution
+
+SELECT
+    EXTRA AS REPORT_REASON,
+    COUNT(distinct post_id) AS REPORT_COUNT
+FROM ACTIONS
+WHERE ACTION='report'
+AND ACTION_DATE=DATE_ADD('2019-07-05',INTERVAL -1 DAY)
+GROUP BY 1
+ORDER BY 2 ASC;
+
+# 2ND SOLUTION
+SELECT extra as report_reason, count(DISTINCT post_id) as report_count
+FROM Actions
+WHERE action = "report" and DATEDIFF("2019-07-05", action_date) = 1
+GROUP BY extra
