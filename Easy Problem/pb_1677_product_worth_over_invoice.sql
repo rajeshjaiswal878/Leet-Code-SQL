7e60277f1866adbd3db85901744b01d0d54d58a8
Rajesh Ashok Jaiswal <rajeshjaiswal878@gmail.com>
2021-02-02 21:28:02 -0800

    Problem 1677 Product's Worth Over Invoices Solution

---
+++ Easy Problem/pb_1677_product_worth_over_invoice.sql
@@ -0,0 +1,61 @@
+/*
+Write an SQL query that will, for all products, return each product name with total amount due, paid, canceled, and refunded across all invoices.
+
+Return the result table ordered by product_name.
+
+The query result format is in the following example:
+
+
+
+Product table:
++------------+-------+
+| product_id | name  |
++------------+-------+
+| 0          | ham   |
+| 1          | bacon |
++------------+-------+
+Invoice table:
++------------+------------+------+------+----------+----------+
+| invoice_id | product_id | rest | paid | canceled | refunded |
++------------+------------+------+------+----------+----------+
+| 23         | 0          | 2    | 0    | 5        | 0        |
+| 12         | 0          | 0    | 4    | 0        | 3        |
+| 1          | 1          | 1    | 1    | 0        | 1        |
+| 2          | 1          | 1    | 0    | 1        | 1        |
+| 3          | 1          | 0    | 1    | 1        | 1        |
+| 4          | 1          | 1    | 1    | 1        | 0        |
++------------+------------+------+------+----------+----------+
+Result table:
++-------+------+------+----------+----------+
+| name  | rest | paid | canceled | refunded |
++-------+------+------+----------+----------+
+| bacon | 3    | 3    | 3        | 3        |
+| ham   | 2    | 4    | 5        | 3        |
++-------+------+------+----------+----------+
+- The amount of money left to pay for bacon is 1 + 1 + 0 + 1 = 3
+- The amount of money paid for bacon is 1 + 0 + 1 + 1 = 3
+- The amount of money canceled for bacon is 0 + 1 + 1 + 1 = 3
+- The amount of money refunded for bacon is 1 + 1 + 1 + 0 = 3
+- The amount of money left to pay for ham is 2 + 0 = 2
+- The amount of money paid for ham is 0 + 4 = 4
+- The amount of money canceled for ham is 5 + 0 = 5
+- The amount of money refunded for ham is 0 + 3 = 3
+
+Problem Link:https://leetcode.com/problems/products-worth-over-invoices/
+*/
+
+# My Solution:
+select
+    p.name,
+    sum(i.rest) as rest,
+    sum(i.paid) as paid,
+    sum(i.canceled) as canceled,
+    sum(i.refunded) as refunded
+from Product p
+inner join Invoice i
+using (product_id)
+group by 1
+order by 1;
+
+#2nd Solution:
+
